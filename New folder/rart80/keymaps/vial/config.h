1743be6956f2b608c6d9f8695a7078af7ac368bb
Alabahuy <rezzaadio@gmail.com>
2022-10-18 09:04:32 +0700

    add

---
+++ New folder/rart80/keymaps/vial/config.h
@@ -0,0 +1,27 @@
+/*
+Copyright 2022 Alabahuy
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 1, 0 }
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 2
+#define VIAL_KEY_OVERRIDE_ENTRIES 4
+
+#define VIAL_KEYBOARD_UID {0xF5, 0xA1, 0x92, 0xBF, 0xA3, 0x33, 0x66, 0x9C}
+
